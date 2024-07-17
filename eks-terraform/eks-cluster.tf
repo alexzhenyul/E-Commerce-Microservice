6ae18f100a736b3e8cb6ea37991147002ac595a7
Alex Liang <115761180+alexzhenyul@users.noreply.github.com>
2024-07-18 00:36:31 +1000

    Update eks-cluster.tf

--- eks-terraform/eks-cluster.tf
+++ eks-terraform/eks-cluster.tf
@@ -4,8 +4,8 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
-  role_arn        = aws_iam_role.devops_iam_role.arn
-  node_role_arn   = aws_iam_role.node_iam_role.arn
+  cluster_endpoint_public_access  = true
+  enable_cluster_creator_admin_permissions = true
 
   enable_irsa = true
 
@@ -28,12 +28,6 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
-    
-    depends_on = [
-    aws_iam_role_policy_attachment.EKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.EKS_CNI_Policy,
-    aws_iam_role_policy_attachment.EC2ContainerRegistryReadOnly,
-    ]  
   }
 }
 
